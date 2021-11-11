538016acd3c97c891ed8e22cef260f3c8ce87610
janetmeng <23jmeng@castilleja.org>
2021-11-11 14:34:00 -0800

    backup - cards working

--- Test2/CardCollectionViewCell.swift
+++ Test2/CardCollectionViewCell.swift
@@ -7,8 +7,29 @@
 
 import UIKit
 
+@objc protocol SymbolProtocol {
+     func didTapImageView(for cell: CardCollectionViewCell)
+}
+
 class CardCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var symbolImageView: UIImageView!
- //   @IBOutlet weak var symbolBigLabel: UILabel!
-   // @IBOutlet weak var symbolNameLabel: UILabel!
+    
+    var gesture: UITapGestureRecognizer
+    weak var delegate: SymbolProtocol?
+      
+    override init(frame: CGRect) {
+        self.gesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        super.init(frame: frame)
+        symbolImageView.isUserInteractionEnabled = true
+        self.symbolImageView.addGestureRecognizer(gesture)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        self.delegate?.didTapImageView(for: self)
+        // print("handleTap")
+    }
 }
