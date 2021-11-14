f001eda08f8ddd424b87ddc2d1eed4c42d05f4fb
janetmeng <23jmeng@castilleja.org>
2021-11-14 00:44:53 -0800

    cards are still showing up correctly, need to figure out how to update the cards based on seeing if the 2 taps that the user tapped were correct or not

--- Test2/CardCollectionViewCell.swift
+++ Test2/CardCollectionViewCell.swift
@@ -7,29 +7,7 @@
 
 import UIKit
 
-@objc protocol SymbolProtocol {
-     func didTapImageView(for cell: CardCollectionViewCell)
-}
-
 class CardCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var symbolImageView: UIImageView!
-    
-    var gesture: UITapGestureRecognizer
-    weak var delegate: SymbolProtocol?
-      
-    override init(frame: CGRect) {
-        self.gesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        super.init(frame: frame)
-        symbolImageView.isUserInteractionEnabled = true
-        self.symbolImageView.addGestureRecognizer(gesture)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    @objc func handleTap(_ sender: UITapGestureRecognizer) {
-        self.delegate?.didTapImageView(for: self)
-        // print("handleTap")
-    }
+    let tapRecognizer1: UITapGestureRecognizer = UITapGestureRecognizer()
 }
---
+++ Test2/SymbolToggleHeaderViewCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  SymbolToggleHeaderViewCollectionReusableView.swift
+//  Test2
+//
+//  Created by Janet Meng on 11/13/21.
+//
+
+import UIKit
+
+class SymbolToggleHeaderViewCollectionReusableView: UICollectionReusableView {
+    @IBOutlet weak var handToggle: UISegmentedControl!
+}
